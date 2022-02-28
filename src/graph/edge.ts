a1117930129868891000a8fb4526cd452963dd72
Alessandro Solbiati <lessandro@fb.com>
2022-02-28 20:21:43 +0000

    [graph] edge, node, interface

---
+++ src/graph/edge.ts
@@ -0,0 +1,8 @@
+import {GameObjectOnGraph, GeometryOnGraph} from '../graph/graph.interface'
+
+export class Edge{
+	constructor(
+		gameObjects: Array<GameObjectOnGraph>,
+		geometries: Array<GeometryOnGraph>
+	){}
+}
---
+++ src/graph/node.ts
@@ -0,0 +1,8 @@
+import {GameObjectOnGraph, GeometryOnGraph} from '../graph/graph.interface'
+
+export class Node{
+	constructor(
+		gameObjects: Array<GameObjectOnGraph>,
+		geometries: Array<GeometryOnGraph>
+	){}
+}
