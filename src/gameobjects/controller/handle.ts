6a416fcf6d796575e28f8ec8457db48cf0c23ae9
Alessandro Solbiati <lessandro@fb.com>
2022-03-01 21:35:52 +0000

    [prettier]

--- src/gameobjects/controller/handle.ts
+++ src/gameobjects/controller/handle.ts
@@ -1,7 +1,7 @@
 import { Point } from "../../geometry/point";
 
-export class Handle extends Phaser.GameObjects.Image{
-	constructor(scene: Phaser.Scene, point: Point){
-		super(scene, point.x, point.y, "controlPoint");
-	}
+export class Handle extends Phaser.GameObjects.Image {
+  constructor(scene: Phaser.Scene, point: Point) {
+    super(scene, point.x, point.y, "controlPoint");
+  }
 }
