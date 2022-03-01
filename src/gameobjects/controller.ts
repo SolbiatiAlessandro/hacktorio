727986b34d8a63498882e2e480573e4d301962a1
Alessandro Solbiati <lessandro@fb.com>
2022-03-01 21:06:32 +0000

    [prettier]

--- src/gameobjects/controller.ts
+++ src/gameobjects/controller.ts
@@ -1,4 +1,4 @@
-import { Constants } from "../constants"; 
+import { Constants } from "../constants";
 
 import { MainScene } from "../scenes/main-scene";
 
@@ -10,34 +10,37 @@ import { NodeGeometries } from "../builders/node-builder";
 
 import { GameObjectOnGraph } from "../interfaces/graph.interface";
 
-
-export class Controller   
-extends Phaser.GameObjects.Group
-implements GameObjectOnGraph
+export class Controller
+  extends Phaser.GameObjects.Group
+  implements GameObjectOnGraph
 {
   _graphParentElement: Node;
 
-	controllerCenter: Phaser.GameObjects.Sprite;
-	rightHandle: Phaser.GameObjects.Sprite;
-	leftHandle: Phaser.GameObjects.Sprite;
-	line: Phaser.GameObjects.Line;
+  controllerCenter: Phaser.GameObjects.Sprite;
+  rightHandle: Phaser.GameObjects.Sprite;
+  leftHandle: Phaser.GameObjects.Sprite;
+  line: Phaser.GameObjects.Line;
 
-	depth: number = 3;
+  depth: number = 3;
 
-	pointCenter(): Point{
-		//@ts-ignore
-		return this.graphParentElement.geometries[NodeGeometries.POINT__CENTER];
-	}
+  pointCenter(): Point {
+    //@ts-ignore
+    return this.graphParentElement.geometries[NodeGeometries.POINT__CENTER];
+  }
 
-	pointLeft(): Point{
-		//@ts-ignore
-		return this.graphParentElement.geometries[NodeGeometries.POINT__LEFT_HANDLE];
-	}
+  pointLeft(): Point {
+    //@ts-ignore
+    return this.graphParentElement.geometries[
+      NodeGeometries.POINT__LEFT_HANDLE
+    ];
+  }
 
-	pointRight(): Point{
-		//@ts-ignore
-		return this.graphParentElement.geometries[NodeGeometries.POINT__RIGHT_HANDLE];
-	}
+  pointRight(): Point {
+    //@ts-ignore
+    return this.graphParentElement.geometries[
+      NodeGeometries.POINT__RIGHT_HANDLE
+    ];
+  }
 
   get graphParentElement(): Node {
     return this._graphParentElement;
@@ -45,40 +48,39 @@ implements GameObjectOnGraph
 
   set graphParentElement(edge: Node) {
     this._graphParentElement = edge;
-		this.populate();
+    this.populate();
   }
 
-	populate(){
-		this.rightHandle = this.create(
-			...this.pointRight().vector(),
-			"controlPoint"
-		);
-		this.leftHandle = this.create(
-			...this.pointLeft().vector(),
-			"controlPoint"
-		);
-		this.createLine();
-		this.controllerCenter = this.create(
-			...this.pointCenter().vector(),
-			"controlPointCenter"
-		);
-		this.setDepth(this.depth);
-	}
-
-	createLine(){
-		this.line = new Phaser.GameObjects.Line(
-      this.scene,
-      0, 0,
-      this.leftHandle.x, this.leftHandle.y,
-      this.rightHandle.x, this.rightHandle.y,
-			Constants.PRIMARY_COLOR, 0.8
+  populate() {
+    this.rightHandle = this.create(
+      ...this.pointRight().vector(),
+      "controlPoint"
     );
-		this.line.setOrigin(0, 0);
-		this.scene.add.existing(this.line);
-		this.add(this.line);
-	}
+    this.leftHandle = this.create(...this.pointLeft().vector(), "controlPoint");
+    this.createLine();
+    this.controllerCenter = this.create(
+      ...this.pointCenter().vector(),
+      "controlPointCenter"
+    );
+    this.setDepth(this.depth);
+  }
 
-	update(){};
+  createLine() {
+    this.line = new Phaser.GameObjects.Line(
+      this.scene,
+      0,
+      0,
+      this.leftHandle.x,
+      this.leftHandle.y,
+      this.rightHandle.x,
+      this.rightHandle.y,
+      Constants.PRIMARY_COLOR,
+      0.8
+    );
+    this.line.setOrigin(0, 0);
+    this.scene.add.existing(this.line);
+    this.add(this.line);
+  }
+
+  update() {}
 }
-
-
