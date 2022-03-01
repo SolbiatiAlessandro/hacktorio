6c97ce538b0654e3765080dd22496dc9a5f970f2
Alessandro Solbiati <lessandro@fb.com>
2022-03-01 20:58:21 +0000

    [controller] line (not working)

--- src/geometry/point.ts
+++ src/geometry/point.ts
@@ -2,8 +2,9 @@ import { GeometryOnGraph } from "../interfaces/graph.interface";
 
 export class Point extends Phaser.Math.Vector2 implements GeometryOnGraph {
   graphParentElement: any;
-  constructor(public x: number, public y: number) {
-    super(x, y);
-  }
   update() {}
+
+	vector(): [number, number]{
+		return [this.x, this.y]
+	}
 }
