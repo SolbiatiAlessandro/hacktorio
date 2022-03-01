f74bf4cd87897d06e093bc58c19682db56f175c4
Alessandro Solbiati <lessandro@fb.com>
2022-03-01 21:34:24 +0000

    [controller] sub controller gameobjects

---
+++ src/gameobjects/controller/line.ts
@@ -0,0 +1,11 @@
+import { Constants } from "../../constants";
+
+import { Handle } from "../../gameobjects/controller/handle";
+
+export class Line extends Phaser.GameObjects.Line{
+	constructor(scene: Phaser.Scene, left: Handle, right: Handle){
+		super(scene, 0, 0, left.x, left.y, right.x, right.y,
+					Constants.PRIMARY_COLOR, 0.8);
+			this.setOrigin(0, 0);
+	}
+}
